d60754d18aad08a9e6748a69e4215d201a2292f3
DESKTOP-5BQEN6A\Sinalicious <h0lyquack.games@gmail.com>
2017-09-14 12:58:16 +0100

    Added all images to the project folder for future use, added all the code for the functionality of the app, tested and working on Benq 5.7 (v4.2.1)

--- app/src/main/java/com/example/sina/a01_android_dice/MainActivity.java
+++ app/src/main/java/com/example/sina/a01_android_dice/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.sina.a01_android_dice;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the id given in the xml to a variable to be used inside Java
+        Button rollButton = (Button) findViewById(R.id.rollButton);
+        final ImageView DiceLeft = (ImageView) findViewById(R.id.imgDiceLeft);
+        final ImageView DiceRight = (ImageView) findViewById(R.id.imgDiceRight);
+
+        // Create an array(list) to store the images for our dices
+        // making it final so it won't be changed (constant variable)
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        // Create event OnClick for the rollButton
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Print the log d = Debug
+                Log.d("Dice", "button pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                // Create number as int to store a random number generated but the function from 0 to 5
+                int number = randomNumberGenerator.nextInt(6);
+
+                DiceLeft.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+
+                DiceRight.setImageResource(diceArray[number]);
+            }
+        });
     }
 }
